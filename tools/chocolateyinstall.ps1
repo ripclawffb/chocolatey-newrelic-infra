5b907e838032ce6dabf4d1d21952e0adf71e1a5f
ripclawffb <ripclaw_ffb@hotmail.com>
2020-05-08 21:39:27 -0700

    Add support for additional install parameters

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,10 +1,41 @@
 ﻿$ErrorActionPreference = 'Stop';
 
+$additionalArgs = ''
+$packageParameters = Get-PackageParameters
+
+# see if any parameters were passed
+if ($packageParameters['GenerateConfig']) { $GenerateConfig = $packageParameters['GenerateConfig'] }
+if ($packageParameters['LicenseKey']) { $LicenseKey = $packageParameters['LicenseKey'] }
+if ($packageParameters['DisplayName']) { $DisplayName = $packageParameters['DisplayName'] }
+if ($packageParameters['Proxy']) { $Proxy = $packageParameters['Proxy'] }
+if ($packageParameters['CustomAttributes']) { $CustomAttributes = $packageParameters['CustomAttributes'] }
+
+if(Get-Variable -Name GenerateConfig -ErrorAction SilentlyContinue) {
+  $additionalArgs = 'GENERATE_CONFIG=true'
+
+  if(Get-Variable -Name LicenseKey -ErrorAction SilentlyContinue) {
+    $additionalArgs += " LICENSE_KEY=$LicenseKey"
+  }
+
+  if(Get-Variable -Name DisplayName -ErrorAction SilentlyContinue) {
+    $additionalArgs += " DISPLAY_NAME=$DisplayName"
+  }
+
+  if(Get-Variable -Name Proxy -ErrorAction SilentlyContinue) {
+    $additionalArgs += " PROXY=$Proxy"
+  }
+
+  if(Get-Variable -Name CustomAttributes -ErrorAction SilentlyContinue) {
+    $additionalArgs += " CUSTOM_ATTRIBUTES=$CustomAttributes"
+  }
+}
+
+
 $packageName     = 'newrelic-infra'
 $softwareName    = 'newrelic-infra*'
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://download.newrelic.com/infrastructure_agent/windows/newrelic-infra.1.8.2.msi'
+$url64      = 'https://download.newrelic.com/infrastructure_agent/windows/newrelic-infra.1.11.37.msi'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -14,10 +45,10 @@ $packageArgs = @{
 
   softwareName  = $softwareName
 
-  checksum64     = '6960abcf204cd3a99ece281b6259136f6d7d56bd4149d5361098241e702c1304'
+  checksum64     = '25f48365e88f166e2451c07ee14e21c69e8ec656283b988abbd1c2ec6e1f236f'
   checksumType64 = 'sha256'
 
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  silentArgs    = "/qn $additionalArgs /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }
 
